7399debe056c21eded16c1e2d420a89e001d1008
Arya Habibi <5auru5@users.noreply.github.com>
2020-08-03 21:44:58 -0500

    Update check.py
    Added User input

--- check.py
+++ check.py
@@ -1,18 +1,22 @@
 import csv
 masterfile = input("Please enter the master file name: ")
+mastercol = input("Please enter the column to check (as a number starting from 1): ")
+mastercol -= 1
 checkfile = input("Please input the cvs to be checked: ")
+checkcol = input("Please enter the column to check (as a number starting from 1): ")
+checkcol -= 1
 with open(masterfile, 'rb') as master:
-    master_indices = dict((r[0], i) for i, r in enumerate(csv.reader(master)))
+    master_indices = dict((r[mastercol], i) for i, r in enumerate(csv.reader(master)))
 
-with open(checkfile, 'rb') as hosts:
+with open(checkfile, 'rb') as check:
     with open('results.csv', 'wb') as results:
-        reader = csv.reader(hosts)
+        reader = csv.reader(check)
         writer = csv.writer(results)
 
-        writer.writerow(next(reader, []) + ['RESULTS'])
+        writer.writerow(next(reader, []) + ['Compare Value'])
 
         for row in reader:
-            index = master_indices.get(row[0])
+            index = master_indices.get(row[checkcol])
             if index is not None:
                 message = 'True'
             else:
