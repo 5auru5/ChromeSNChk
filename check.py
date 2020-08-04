da2856b53a6772269713a26a3d7e1792e17c0a99
Arya Habibi <5auru5@users.noreply.github.com>
2020-08-03 22:14:35 -0500

    Update check.py
    error checking

--- check.py
+++ check.py
@@ -1,9 +1,26 @@
 import csv
-masterfile = input("Please enter the master file name: ")
-mastercol = input("Please enter the column to check (as a number starting from 1): ")
-mastercol -= 1
-checkfile = input("Please input the cvs to be checked: ")
-checkcol = input("Please enter the column to check (as a number starting from 1): ")
+while True:
+    fileType = input("Please Select a File Type\n 1: CSV\n 2: TSV\n Selection: ")
+    try:
+        filetype = int(fileType)
+    except:
+        print("Please user numeric digits.")
+        continue
+    if int(fileType) > 2 or int(fileType) < 0:
+        print("Please enter a valid number")
+        continue
+    else:
+        break
+print(str(fileType))
+fileType = int(fileType)
+if fileType == 1:
+    extension = ".csv"
+if fileType == 2:
+    extension = ".tsv"
+masterfile = input("Please enter the master file name: ") + extension
+mastercol = int(input("Please enter the column to check (as a number starting from 1): ")) - 1
+checkfile = input("Please input the csv to be checked: ") + extension
+checkcol = int(input("Please enter the column to check (as a number starting from 1): ")) - 1
 checkcol -= 1
 with open(masterfile, 'rb') as master:
     master_indices = dict((r[mastercol], i) for i, r in enumerate(csv.reader(master)))
